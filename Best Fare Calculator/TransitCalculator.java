5a6a7c22de1149200271c6f49c0cb3c5aa7506e1
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-06-20 03:08:43 +0530

    Update TransitCalculator.java

--- Best Fare Calculator/TransitCalculator.java
+++ Best Fare Calculator/TransitCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class TransitCalculator
+class TransitCalculator
 {
     
     String[] fareOptions = {"Pay-per-ride (single ride): $2.75", "7-day Unlimited Rides: $33.00", "30-day Unlimited Rides: $127.00"}; // stores the fare options available to choose from
@@ -13,22 +13,26 @@ public class TransitCalculator
     int days; // number of days the person takes rides
     int rides; // number of rides the person takes
     int age; // age of the person
+    boolean disability = false; // person has a disability or not
 
-    public TransitCalculator(int numberOfDays, int numberOfRides, int ageOfPerson)
+    TransitCalculator(int numberOfDays, int numberOfRides, int ageOfPerson, boolean disable)
     {
         this.days = numberOfDays;
         this.rides = numberOfRides;
         this.age = ageOfPerson;
+
+        if (disable)
+            this.disability = disable;
     }
 
     // finds the cost per ride for the 2nd fare option i.e 7-day Unlimited Rides: $33.00
     public double unlimited7Price()
     {
-        if (age < 65)
-            return (Math.ceil((double)days / 7) * fares[1]) / rides;
+        if (age >= 65 || disability)
+            return (Math.ceil((double)days / 7) * elderlyFares[1]) / rides;
 
         else
-            return (Math.ceil((double)days / 7) * elderlyFares[1]) / rides;
+            return (Math.ceil((double)days / 7) * fares[1]) / rides;
     }
 
     // finds the price per ride for all the 3 fare options
@@ -36,18 +40,18 @@ public class TransitCalculator
     {
         double[] pricePerRide = new double[3];
 
-        if (age < 65)
-        {
-            pricePerRide[0] = fares[0];
-            pricePerRide[1] = unlimited7Price();
-            pricePerRide[2] = fares[2] / rides;
-        }
-
-        else
+        if (age >= 65 || disability)
         {
             pricePerRide[0] = elderlyFares[0];
             pricePerRide[1] = unlimited7Price();
             pricePerRide[2] = elderlyFares[2] / rides;
+        }
+
+        else
+        {
+            pricePerRide[0] = fares[0];
+            pricePerRide[1] = unlimited7Price();
+            pricePerRide[2] = fares[2] / rides;
         }
 
 //        System.out.println(Arrays.toString(pricePerRide));
@@ -66,23 +70,24 @@ public class TransitCalculator
                 minIndex = i;
         }
 
-        if (age < 65)
-            return "The best option for you is " + fareOptions[minIndex] + " at a cost of " + Math.round(possiblePrices[minIndex] * 100.0) / 100.0 + " per ride.";
-        else
+        if (age >= 65 || disability)
             return "The best option for you is " + elderlyFareOptions[minIndex] + " at a cost of " + Math.round(possiblePrices[minIndex] * 100.0) / 100.0 + " per ride.";
+        else
+            return "The best option for you is " + fareOptions[minIndex] + " at a cost of " + Math.round(possiblePrices[minIndex] * 100.0) / 100.0 + " per ride.";
     }
 
     @Override
     public String toString()
     {
-        return "You will ride " + rides + " times in " + days + " days (Age: " + age + ")\n";
+        return "You will ride " + rides + " times in " + days + " days (Age: " + age + " and Disability: " + disability + ")\n";
     }
 
     public static void main(String[] args)
     {
-        TransitCalculator person1 = new TransitCalculator(12, 82, 60);
-        TransitCalculator person2 = new TransitCalculator(12, 82, 90);
-        TransitCalculator person3 = new TransitCalculator(23, 16, 42);
+        TransitCalculator person1 = new TransitCalculator(12, 82, 60, true);
+        TransitCalculator person2 = new TransitCalculator(12, 82, 90, false);
+        TransitCalculator person3 = new TransitCalculator(23, 16, 42, false);
+        TransitCalculator person4 = new TransitCalculator(12, 82, 65, true);
 
         System.out.print(person1);
         System.out.println(person1.getBestFare());
@@ -92,6 +97,9 @@ public class TransitCalculator
 
         System.out.print(person3);
         System.out.println(person3.getBestFare());
+
+        System.out.print(person4);
+        System.out.println(person4.getBestFare());
     }
 
 }
