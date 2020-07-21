f2ded8a8bb10fde3e5d152df1557a5f39abb86bf
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-07-21 19:39:22 +0530

    Create the_waiting_game.py

---
+++ GAMES/The Accurately-Waiting Game/the_waiting_game.py
@@ -0,0 +1,24 @@
+import random
+import time
+
+def waiting_game():
+    
+    goal_time = random.choice([2, 2.5, 3, 3.5, 4])
+    print("Your goal is", goal_time, "seconds.\n")
+    
+    input("----Press Enter key to begin----\n")
+    start = time.perf_counter()
+    
+    input("...Press Enter key after " + str(goal_time) + " seconds...\n")
+    elapsed_time = time.perf_counter() - start
+    print("\nTime Elapsed:", round(elapsed_time, 3))
+    
+    if elapsed_time == goal_time:
+        print("Wow! Superb Timing.")
+    elif elapsed_time < goal_time:
+        print("(Ahh!", round(goal_time - elapsed_time, 3), "seconds too fast.)")
+    else:
+        print("(Ahh!", round(elapsed_time - goal_time, 3), "seconds too slow.)")
+        
+        
+waiting_game()
