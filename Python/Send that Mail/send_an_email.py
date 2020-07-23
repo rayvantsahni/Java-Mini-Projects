a17f1845a2ed761956fba4bd641a9d5c9a8a18cc
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-07-23 18:31:57 +0530

    Update send_an_email.py

--- Python/Send that Mail/send_an_email.py
+++ Python/Send that Mail/send_an_email.py
@@ -5,7 +5,6 @@ sender_password = "my_password"
 
 def send_email(receiver_email, subject, body):
     message = "Subject: " + str(subject) + "\n\n" + str(body)
-    #message = "Subject: {}\n\n{}".format(subject, body)
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(sender_email, sender_password)
         server.sendmail(sender_email, receiver_email, message)
