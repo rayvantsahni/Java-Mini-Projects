50164952d348b595335df0546674293a0a83e49d
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-08 18:46:10 +0530

    Create snow_man.cpp

---
+++ C++/sUnow Man/snow_man.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+  cout << "    *                   *        \n";
+  cout << "*        *                       \n";
+  cout << " *                *            * \n";
+  cout << "      *     *                    \n";
+  cout << "                      *          \n";
+  cout << "*         *                 *    \n";
+  cout << "     * 	       *     *          \n";
+  cout << "            HHHHH        *       \n";
+  cout << " *         HHHHHHH               \n";
+  cout << "     *   HHHHHHHHHHH    *     *  \n";
+  cout << "         *   0 0   *             \n";
+  cout << "        *     -     *            \n";
+  cout << "  *       *       *         *    \n";
+  cout << "         SSS * * S               \n";
+  cout << "         * SSSSSSSS *            \n";
+  cout << "       *      O   SS  *          \n";
+  cout << "      *       O    S   *         \n";
+  cout << "      *       O        *         \n";
+  cout << "       *              *          \n";
+  cout << "           *   *   *             \n";
+
+  return 0;
+
+}
